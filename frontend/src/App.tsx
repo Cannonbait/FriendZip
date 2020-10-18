74a26bcce1a8737d63d820f3ee28abbf700bc606
Ivar Josefsson <ivar.josefsson@visma.com>
2020-10-18 16:56:37 +0200

    stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 import "./App.css";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import * as queryString from "querystring";
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID!;
 
@@ -13,27 +15,91 @@ const SCOPES = [
   "https://www.googleapis.com/auth/calendar.readonly",
 ].join(" ");
 
-function getUsers(): Promise<any> {
-  return fetch("http://localhost:8080/users").then((r) => r.json());
+interface Friend {
+  id: string;
+  name: string;
 }
 
+function getUsers(): Promise<Friend[]> {
+  return fetch("http://localhost:8080/friends").then((r) => r.json());
+}
+
+function sendPlanRequest(
+  accessToken: string,
+  userId: string,
+  friendId: string
+): Promise<PlansResponse> {
+  return fetch("http://localhost:8080/alskdh", {
+    method: "POST",
+    body: JSON.stringify({ userId, friendId }),
+  }).then((r) => r.json());
+}
+
+interface PlansResponse {}
+
 function SuccessPage() {
-  // TODO Call backend to fetch calendars
-  const data = useQuery("users", getUsers);
-  console.log(data.data);
+  const [friendId, setFriendId] = useState<string | null>(null);
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [response, setResponse] = useState<PlansResponse | null>(null);
+
+  const { data, isLoading } = useQuery("users", getUsers);
+
+  const handleSubmit = async () => {
+    const userId = window.localStorage.getItem("user_id");
+    const accessToken = window.localStorage.getItem("token");
+
+    if (userId && accessToken && friendId) {
+      setSubmitting(true);
+      const plansResponse = await sendPlanRequest(
+        accessToken,
+        userId,
+        friendId
+      );
+      setResponse(plansResponse);
+      console.log(plansResponse);
+
+      setSubmitting(false);
+    }
+  };
+
   return (
     <div>
       You are signed in!
-      <div>Please select the calendar you want to plan around: </div>
-      <select>
-        <option>Tjena</option>
+      <div>Please select a friend: </div>
+      <select onChange={(event) => setFriendId(event.target.value)}>
+        {data?.map((friend) => {
+          return (
+            <option value={friend.id} key={friend.id}>
+              {friend.name}
+            </option>
+          );
+        })}
       </select>
+      {isSubmitting && "IsSubmittingBoi!!!"}
+      <button onClick={handleSubmit}>Make plans!</button>
     </div>
   );
 }
 
 function Callback() {
-  // TODO Call backend API with access_token
+  useEffect(() => {
+    const fn = async () => {
+      const { code } = queryString.parse(window.location.search.substr(1));
+      const tokenResponse = await fetch(
+        `http://localhost:8080/authenticate?accessCode=${code}`,
+        {
+          method: "POST",
+        }
+      ).then((r) => r.json());
+
+      const token = jwtDecode(tokenResponse.id_token) as { sub: string };
+
+      window.localStorage.setItem("user_id", token.sub);
+      window.localStorage.setItem("token", tokenResponse.access_token);
+      window.location.href = "/success";
+    };
+    fn();
+  }, []);
   return <div>Callback</div>;
 }
 

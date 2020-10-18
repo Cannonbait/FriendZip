85c07a9db672a800b76bca11328fab81c0de9af2
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2020-10-18 11:25:17 +0200

    Initial backend commit

---
+++ backend/src/main/kotlin/se/coolaganget/friendzip/FriendzipApplication.kt
@@ -0,0 +1,11 @@
+package se.coolaganget.friendzip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FriendzipApplication
+
+fun main(args: Array<String>) {
+	runApplication<FriendzipApplication>(*args)
+}

4d1645a6113e84ea38d56f610de855aea39648dd
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2020-10-18 11:43:15 +0200

    Replace backend with java backend

---
+++ backend/src/main/java/se/coolaganget/friendzip/FriendzipApplication.java
@@ -0,0 +1,13 @@
+package se.coolaganget.friendzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendzipApplication.class, args);
+	}
+
+}
